54f57671e9af7972d78266f88e9910d51a530b21
IT14KR <131267129+IT14KR@users.noreply.github.com>
2024-05-22 16:35:21 +0200

    add commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
-import morgan from "morgan";
+import logger from "morgan";
 import cors from "cors";
+import "dotenv/config";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
 
-app.use(morgan("tiny"));
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +24,4 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+export default app;
